6d197aac4fd215564070f731462e3cac55d23bee
Twilight-1 <104142951+Twilight-1@users.noreply.github.com>
2024-12-16 09:16:41 +0300

    Add files via upload

---
+++ Lab 4/calculator.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types=1);
+
+/*
+ЗАДАНИЕ 1
+- Проверьте, была ли корректно отправлена форма
+- Если она была отправлена, отфильтруйте полученные значения
+- В зависимости от оператора производите различные математические действия
+- В случае деления, проверьте, делитель на равенство с нулем (на ноль делить нельзя)
+- Сохраните полученный результат вычисления в переменной
+*/
+
+$result = null; // Переменная для хранения результата
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Фильтруем входные данные
+    $num1 = filter_var(trim($_POST['num1']), FILTER_VALIDATE_FLOAT);
+    $num2 = filter_var(trim($_POST['num2']), FILTER_VALIDATE_FLOAT);
+    $operator = $_POST['operator'];
+
+    if ($num1 !== false && $num2 !== false) {
+        // Выполняем соответствующую математическую операцию в зависимости от оператора
+        switch ($operator) {
+            case '+':
+                $result = $num1 + $num2;
+                break;
+            case '-':
+                $result = $num1 - $num2;
+                break;
+            case '*':
+                $result = $num1 * $num2;
+                break;
+            case '/':
+                if ($num2 == 0) {
+                    $result = 'Ошибка: деление на ноль недопустимо.';
+                } else {
+                    $result = $num1 / $num2;
+                }
+                break;
+            default:
+                $result = 'Некорректный оператор.';
+                break;
+        }
+    } else {
+        $result = 'Ошибка: некорректные числа.';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Калькулятор</title>
+</head>
+<body>
+
+<h1>Калькулятор</h1>
+
+<?php
+/*
+ЗАДАНИЕ 2
+- Если результат существует, выведите его
+*/
+if ($result !== null) {
+    echo "<h2>Результат: " . htmlspecialchars((string)$result) . "</h2>";
+}
+?>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+
+<p><label for="num1">Число 1</label><br>
+<input type="text" name="num1" id="num1" required></p>
+
+<p><label for="operator">Оператор</label><br>
+<select name="operator" id="operator">
+    <option value="+" selected>+</option>
+    <option value="-">-</option>
+    <option value="*">*</option>
+    <option value="/">/</option>
+</select></p>
+
+<p><label for="num2">Число 2</label><br>
+<input type="text" name="num2" id="num2" required></p>
+
+<button type="submit">Считать!</button>
+
+</form>
+
+</body>
+</html>
