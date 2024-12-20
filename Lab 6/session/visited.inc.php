930b641382eaeaf20665e41bfc4ff56c1e780d32
Twilight-1 <104142951+Twilight-1@users.noreply.github.com>
2024-12-20 13:04:32 +0300

    Update visited.inc.php

--- Lab 6/session/visited.inc.php
+++ Lab 6/session/visited.inc.php
@@ -1,24 +1,21 @@
 <?php
-declare(strict_types=1);
-
-/**
- * Показывает список всех посещенных страниц.
- *
- * @return void
- */
-function displayVisitedPages(): void {
-    if (isset($_SESSION['visited_pages']) && is_array($_SESSION['visited_pages'])) {
-        echo "<h3>Список посещённых страниц:</h3>";
-        echo "<ul>";
-        foreach ($_SESSION['visited_pages'] as $page) {
-            echo "<li>" . htmlspecialchars($page) . "</li>";
-        }
-        echo "</ul>";
-    } else {
-        echo "<p>Вы еще не посетили никаких страниц.</p>";
+// Код для всех страниц - вывод информации о посещенных страницах
+echo "<pre>";
+if (isset($_SESSION['visited_pages'])) {
+    foreach ($_SESSION['visited_pages'] as $page) {
+        echo $page ?  htmlspecialchars($page) . "\n" : '';
     }
 }
+else{
+    echo "Массив не найден";
+}
+echo "</pre>";
+/*
+ЗАДАНИЕ 2
+- В случае сохранения данных 
+	- в массив, проверьте, существует ли он в сессии
+	- в строку, преобразуйте её в массив
+- Выводите в цикле список всех посещённых пользователем страниц
 
-// Вызов функции
-displayVisitedPages();
+*/
 ?>
