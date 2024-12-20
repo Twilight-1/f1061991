da9bd01f25d6b6d9ae84bd3e7a2b20d92ec2bb99
Twilight-1 <104142951+Twilight-1@users.noreply.github.com>
2024-12-20 13:47:57 +0300

    Delete Lab 8/config.php

--- Lab 8/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_NAME', 'f1061991_login'); // Имя базы данных
-define('DB_USER', 'f1061991_log'); // Имя пользователя
-define('DB_PASSWORD', 'uuRcMOXs'); // Пароль пользователя
-define('DB_HOST', 'localhost'); // Хост базы данных
-define('DB_CHARSET', 'utf8'); // Кодировка
-?>
