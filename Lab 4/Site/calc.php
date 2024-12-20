324282e5c248f302c347ae70b92cddee7934984b
Twilight-1 <104142951+Twilight-1@users.noreply.github.com>
2024-12-20 12:45:43 +0300

    Update calc.php

--- Lab 4/Site/calc.php
+++ Lab 4/Site/calc.php
@@ -1,18 +1,69 @@
-    <!-- Область основного контента -->
-    <form action=''>
-      <label>Число 1:</label>
-      <br>
-      <input name='num1' type='text'>
-      <br>
-      <label>Оператор: </label>
-      <br>
-      <input name='operator' type='text'>
-      <br>
-      <label>Число 2: </label>
-      <br>
-      <input name='num2' type='text'>
-      <br>
-      <br>
-      <input type='submit' value='Считать'>
-    </form>
-    <!-- Область основного контента -->
+<!-- Область основного контента -->
+<?php
+/*
+ЗАДАНИЕ 1
+- Проверьте, была ли корректно отправлена форма
+- Если она была отправлена, отфильтруйте полученные значения
+- В зависимости от оператора производите различные математические действия
+- В случае деления, проверьте, делитель на равенство с нулем (на ноль делить нельзя)
+- Сохраните полученный результат вычисления в переменной
+*/
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $num1 = filter_input(INPUT_POST, 'num1', FILTER_VALIDATE_FLOAT);
+    $num2 = filter_input(INPUT_POST, 'num2', FILTER_VALIDATE_FLOAT);
+    $operator = filter_input(INPUT_POST, 'operator', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    switch ($operator) {
+        case '+':
+            $result = $num1 + $num2;
+            break;
+        case '-':
+            $result = $num1 - $num2;
+            break;
+        case '*':
+            $result = $num1 * $num2;
+            break;
+        case '/':
+            if ($num2 == 0) {
+                $result = "Ошибка: деление на ноль";
+            } else {
+                $result = $num1 / $num2;
+            }
+            break;
+        default:
+            $result = "Ошибка: некорректный оператор";
+    }
+}
+
+/*
+ЗАДАНИЕ 2
+- Если результат существует, выведите его
+*/
+
+if (isset($result)) {
+    echo "<p>Результат: " . $result . "</p>";
+}
+?>
+
+<form action="http://f1035596.xsph.ru/lab4/site/index.php?id=calc" method="post">
+
+<p><label for="num1">Число 1</label><br>
+<input type="text" name="num1" id="num1" required></p>
+
+<p><label for="operator">Оператор</label><br>
+<select name="operator" id="operator">
+    <option value="+" selected >+</option>
+    <option value="-">-</option>
+    <option value="*">*</option>
+    <option value="/">/</option>
+</select></p>
+
+<p><label for="num2">Число 2</label><br>
+<input type="text" name="num2" id="num2" required></p>
+
+<button type="submit">Считать!</button>
+
+</form>
+
+<!-- Область основного контента -->
